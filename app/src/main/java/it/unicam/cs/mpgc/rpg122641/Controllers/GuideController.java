82f4696a747b0f22d24986076370434e0d115f02
Ilario Perticari <ilario.perticari@studenti.unicam.it>
2026-07-06 19:23:51 +0200

    Creazione della guida del gioco e schermata delle stanze da cui partire

--- app/src/main/java/it/unicam/cs/mpgc/rpg122641/Controllers/GuideController.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg122641/Controllers/GuideController.java
@@ -1,30 +1,32 @@
 package it.unicam.cs.mpgc.rpg122641.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.scene.Node;
 
-public class StartController {
+public class GuideController {
+
+    @FXML
+    private void startGame(ActionEvent event) {
+        goToGame(event);
+    }
+
 
     @FXML
     private void goToGame(ActionEvent event) {
         try {
-            // carica seconda pagina
-            Parent root = FXMLLoader.load(getClass().getResource("/view/guida-view.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/view/room-view.fxml"));
 
-            // prende lo stage attuale
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-            // cambia scena
 
             double w = stage.getScene().getWidth();
             double h = stage.getScene().getHeight();
 
-            stage.setScene(new Scene(root, w, h));
+            stage.setScene(new Scene(root));
             stage.show();
 
         } catch (Exception e) {
---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg122641/Controllers/RoomController.java
@@ -0,0 +1,44 @@
+package it.unicam.cs.mpgc.rpg122641.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class RoomController{
+
+    @FXML
+    private void scenario1() {
+        System.out.println("Hai selezionato lo scenario 1");
+        // TODO: apri lo scenario 1
+    }
+
+    @FXML
+    private void scenario2() {
+        System.out.println("Hai selezionato lo scenario 2");
+    }
+
+    @FXML
+    private void scenario3() {
+        System.out.println("Hai selezionato lo scenario 3");
+    }
+
+    @FXML
+    private void scenario4() {
+        System.out.println("Hai selezionato lo scenario 4");
+    }
+
+    @FXML
+    private void scenario5() {
+        System.out.println("Hai selezionato lo scenario 5");
+    }
+
+    @FXML
+    private void scenario6() {
+        System.out.println("Hai selezionato lo scenario 6");
+    }
+
+    @FXML
+    private void scenario7() {
+        System.out.println("Hai selezionato lo scenario 7");
+    }
+}
