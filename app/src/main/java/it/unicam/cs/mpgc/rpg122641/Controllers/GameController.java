e0f3b521c9657bb1bcdc17f038215a65abc04adf
Ilario Perticari <ilario.perticari@studenti.unicam.it>
2026-07-07 12:42:37 +0200

    Aggiunta interfaccia del gioco dopo selzione luogo

---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg122641/Controllers/GameController.java
@@ -0,0 +1,12 @@
+package it.unicam.cs.mpgc.rpg122641.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class GameController {
+
+    @FXML
+    private void evocaRaziel(ActionEvent event) {
+        System.out.println("Stai evocando raziel");
+    }
+}
--- app/src/main/java/it/unicam/cs/mpgc/rpg122641/Controllers/RoomController.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg122641/Controllers/RoomController.java
@@ -1,21 +1,43 @@
 package it.unicam.cs.mpgc.rpg122641.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
+import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
 public class RoomController{
 
     @FXML
-    private void scenario1() {
-        System.out.println("Hai selezionato lo scenario 1");
-        // TODO: apri lo scenario 1
+    private void scenario1(MouseEvent event) {
+
+        // qui devo:
+        // caricare immagine del demone;
+        // recuperare le frasi
+        // attacco e difesa del demone
+        //recuperare l'oggetto magico
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/game-view.fxml"));
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setFullScreen(true);
+
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @FXML
-    private void scenario2() {
-        System.out.println("Hai selezionato lo scenario 2");
-    }
+    private void scenario2() {System.out.println("Hai selezionato lo scenario 2"); }
 
     @FXML
     private void scenario3() {
@@ -41,4 +63,6 @@ public class RoomController{
     private void scenario7() {
         System.out.println("Hai selezionato lo scenario 7");
     }
+
+
 }
