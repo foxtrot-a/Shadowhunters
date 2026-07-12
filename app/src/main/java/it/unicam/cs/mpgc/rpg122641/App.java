87509751d740cc1404e84b027d3a3ed06a19cdcb
Ilario Perticari <ilario.perticari@studenti.unicam.it>
2026-07-12 13:21:54 +0200

    Revisione codice 3

--- app/src/main/java/it/unicam/cs/mpgc/rpg122641/App.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg122641/App.java
@@ -7,43 +7,43 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 public class App extends Application {
 
-    private static boolean old;
-
-    public Game inizializzazione(){
-        // recuperiamo le configurazioni memorizzate del gioco dal file json
-        Game game = new Game(); // game è l'oggetto che ha tutte le info del gioco
-        Stato par = new Stato();
-        par = (Stato) Persistenza.recupera(par,"statoPartita.json");
-        if (par.isPartitaSalvata()){
-            game = (Game) Persistenza.recupera(game,"partita.json"); // recuperiamo i dati della partita precedente
-       old =true;
-        }else{
-            game = (Game) Persistenza.recupera(game,"gioco.json");  // partita nuova
-        }
-        return game;
-    }
+    private Game game;
+    private boolean partitaSalvata;
 
     @Override
     public void start(Stage stage) throws Exception {
 
+        game = loadGame();
 
-        // una volta caricato il gioco, si parte!
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main-view.fxml"));
-
         Scene scene = new Scene(loader.load(), 600, 700);
 
         StartController controller = loader.getController();
-        controller.setGame(this.inizializzazione()); // passiamo l'oggeto gioco che varrà per tutti i controller
-        controller.setBtn(old);
+        controller.setGame(game);
+        controller.setBtn(partitaSalvata);
+
         stage.setTitle("Shadowhunters");
         stage.setScene(scene);
         stage.show();
     }
 
+    private Game loadGame() {
+
+        Stato stato = (Stato) Persistenza.recupera(new Stato(), "statoPartita.json");
+
+        partitaSalvata = stato.isPartitaSalvata();
+
+        if (partitaSalvata) {
+            return (Game) Persistenza.recupera(new Game(), "partita.json");
+        }
+
+        return (Game) Persistenza.recupera(new Game(), "gioco.json");
+    }
+
     public static void main(String[] args) {
-        old = false;
-        launch();
+        launch(args);
     }
 }
